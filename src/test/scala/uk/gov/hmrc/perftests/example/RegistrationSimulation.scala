8d2466cf77c4e6b39ab3b8a33fb59dc423bf4fa9
aca2492 <209471130+aca2492@users.noreply.github.com>
2025-10-24 17:06:25 +0100

    Creation of navigation to auth page

--- src/test/scala/uk/gov/hmrc/perftests/example/RegistrationSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/example/RegistrationSimulation.scala
@@ -17,15 +17,11 @@
 package uk.gov.hmrc.perftests.example
 
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
-import uk.gov.hmrc.perftests.example.ExampleRequests._
+import uk.gov.hmrc.perftests.example.RegistrationRequests._
 
 class RegistrationSimulation extends PerformanceTestRunner {
 
-  setup("home-page", "Home Page") withRequests navigateToHomePage
-
-  setup("post-vat-return-period", "Post vat return period") withRequests postVatReturnPeriod
-
-  setup("get-turnover-page", "Get turnover page") withRequests getTurnoverPage
+  setup("register-your-company-page", "Register Page") withRequests navigateToAuthStubPage
 
   runSimulation()
 }
