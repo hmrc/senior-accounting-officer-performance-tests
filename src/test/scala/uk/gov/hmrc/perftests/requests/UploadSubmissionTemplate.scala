571dc95a10f5c95ada879747a17862cc95683069
Michael-A-NC <222641308+Michael-A-NC@users.noreply.github.com>
2026-02-12 12:25:06 +0000

    Adds missing import

--- src/test/scala/uk/gov/hmrc/perftests/requests/UploadSubmissionTemplate.scala
+++ src/test/scala/uk/gov/hmrc/perftests/requests/UploadSubmissionTemplate.scala
@@ -1,7 +1,9 @@
 package uk.gov.hmrc.perftests.requests
 
+import io.gatling.core.Predef._
 import io.gatling.core.Predef.find2Validate
 import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.http.Predef._
 import io.gatling.http.Predef.{http, status}
 import uk.gov.hmrc.perftests.support.GatlingSupport.convertHttpActionToSeq
 import uk.gov.hmrc.perftests.support.RequestSupport.{notificationStartPageUrl, registrationPageUrl}
